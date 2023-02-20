87f356c468572740ed8edaf130ead51d05fc6061
damir <dimayu82@gmail.com>
2023-02-20 10:21:15 +0300

    Configuration for Babel

--- webpack.config.js
+++ webpack.config.js
@@ -103,14 +103,7 @@ module.exports = {
       {
         test: /\.m?js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', { targets: "defaults" }]
-            ]
-          }
-        },
+        use: 'babel-loader',
       }
     ],
   },
